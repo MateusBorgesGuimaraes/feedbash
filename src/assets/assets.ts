59d5f4d302c69b25fcf2ea7012c9db49c7a4f1ed
MBguimaraes <104425878+MateusBorgesGuimaraes@users.noreply.github.com>
2024-05-28 16:19:54 -0300

    Add assets

---
+++ src/assets/assets.ts
@@ -0,0 +1,108 @@
+// ICONS
+
+import arrowDownSubMenu from "./icons/arrow-down-menu-icon.svg";
+import confoundedEmoji from "./icons/confounded-emoji-face-icon.svg";
+import darkLogo from "./icons/dark-logo-icon.svg";
+import decTitlePurple from "./icons/dec-title-ligth-purple-icon.svg";
+import decTitleBlue from "./icons/dec-title-ligth-blue-icon.svg";
+import drawSubmenu from "./icons/draw-submenu-icon.svg";
+import editComment from "./icons/edit-comment-icon.svg";
+import emptyStar from "./icons/empty-star-icon.svg";
+import excludeBlue from "./icons/exclude-blue-comment-icon.svg";
+import excludePurple from "./icons/exclude-purple-comment-icon.svg";
+import expand from "./icons/expand-icon.svg";
+import fullStar from "./icons/full-star-icon.svg";
+import halfStar from "./icons/half-star-icon.svg";
+import home from "./icons/home-icon.svg";
+import ligthLogo from "./icons/light-logo-icon.svg";
+import linkBlue from "./icons/link-blue-icon.svg";
+import linkPurple from "./icons/link-purple-icon.svg";
+import logoutIcon from "./icons/logout-icon.svg";
+import partyingEmoji from "./icons/partying-emoji-face-icon.svg";
+import pjotoSubmenu from "./icons/photo-submenu-icon.svg";
+import rating from "./icons/rating-icon.svg";
+import relievedEmoji from "./icons/relieved-emoji-face-icon.svg";
+import report from "./icons/report-icon.svg";
+import save from "./icons/save-icon.svg";
+import smilingEmoji from "./icons/smiling-face-emoji-face-icon.svg";
+import supriseEmoji from "./icons/surprise-emoji-face-icon.svg";
+import thinkingEmoji from "./icons/thiking-emoji-face-icon.svg";
+import twoStarDec from "./icons/two-start-dec-icon.svg";
+import unsave from "./icons/unsave-icon.svg";
+import user from "./icons/user-icon.svg";
+import users from "./icons/users-icon.svg";
+import videoSubmenu from "./icons/video-submenu-icon.svg";
+import warning from "./icons/warning-icon.svg";
+import warningDarkBlue from "./icons/warning-darkBlue-icon.svg";
+import writeSubmenu from "./icons/write-submenu-icon.svg";
+
+//IMAGES
+
+import addPost from "./images/add-post-img.jpg";
+import art from "./images/art-img.jpg";
+import dashboard from "./images/dashboard-img.jpg";
+import hero from "./images/hero-img.jpg";
+import login from "./images/login-resister-img.jpg";
+import logout from "./images/logout-img.jpg";
+import photo from "./images/photo-img.jpg";
+import saveCritics from "./images/save-critics-image.jpg";
+import userCritics from "./images/user-critics-image.jpg";
+import userPosts from "./images/user-posts-img.jpg";
+import userTest1 from "./images/user-test-1.jpg";
+import userTest2 from "./images/user-test-2.jpg";
+import videos from "./images/videos-img.jpg";
+import write from "./images/write-img.jpg";
+
+//EXPORTS
+
+export const assets = {
+  arrowDownSubMenu,
+  confoundedEmoji,
+  darkLogo,
+  decTitlePurple,
+  decTitleBlue,
+  drawSubmenu,
+  editComment,
+  emptyStar,
+  excludeBlue,
+  excludePurple,
+  expand,
+  fullStar,
+  halfStar,
+  home,
+  ligthLogo,
+  linkBlue,
+  linkPurple,
+  logoutIcon,
+  partyingEmoji,
+  pjotoSubmenu,
+  rating,
+  relievedEmoji,
+  report,
+  save,
+  smilingEmoji,
+  supriseEmoji,
+  thinkingEmoji,
+  twoStarDec,
+  unsave,
+  user,
+  users,
+  videoSubmenu,
+  warning,
+  warningDarkBlue,
+  writeSubmenu,
+  addPost,
+  art,
+  dashboard,
+  hero,
+  login,
+  logout,
+  photo,
+  saveCritics,
+  userCritics,
+  userPosts,
+  userTest1,
+  userTest2,
+  videos,
+  write,
+};
